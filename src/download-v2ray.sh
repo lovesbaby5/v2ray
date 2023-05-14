95fdfdae53a61a72ab9812f8e168532f22545d26
lovesbaby5 <133594538+lovesbaby5@users.noreply.github.com>
2023-05-15 01:48:08 +0800

    .

--- src/download-v2ray.sh
+++ src/download-v2ray.sh
@@ -1,6 +1,6 @@
 _get_latest_version() {
-	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
-
+	v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2fly/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
+	echo $v2ray_latest_ver
 	if [[ ! $v2ray_latest_ver ]]; then
 		echo
 		echo -e " $red获取 V2Ray 最新版本失败!!!$none"
@@ -18,8 +18,8 @@ _download_v2ray_file() {
 	[[ -d /tmp/v2ray ]] && rm -rf /tmp/v2ray
 	mkdir -p /tmp/v2ray
 	v2ray_tmp_file="/tmp/v2ray/v2ray.zip"
-	v2ray_download_link="https://github.com/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
-
+	v2ray_download_link="https://github.com/v2fly/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-${v2ray_bit}.zip"
+	echo $v2ray_download_link
 	if ! wget --no-check-certificate -O "$v2ray_tmp_file" $v2ray_download_link; then
 		echo -e "
         $red 下载 V2Ray 失败啦..可能是你的 VPS 网络太辣鸡了...请重试...$none
